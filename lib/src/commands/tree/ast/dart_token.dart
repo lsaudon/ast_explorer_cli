9b417858c5aaac20994f43ae841e9f27856661b9
lsaudon <lsaudon@gmail.com>
2023-03-01 13:24:07 +0100

    ✨ Show method name

--- lib/src/commands/tree/ast/dart_token.dart
+++ lib/src/commands/tree/ast/dart_token.dart
@@ -1,8 +1,13 @@
+import 'package:analyzer/dart/ast/token.dart';
 import 'package:ast_explorer_cli/src/commands/tree/ast/dart_syntatic_entity.dart';
 
 class DartToken extends DartSyntacticEntity {
   const DartToken(super.entity);
 
+  Token get _entity => entity as Token;
+
+  String get value => _entity.lexeme;
+
   @override
-  Map<String, Object?> toMap() => {};
+  Map<String, Object?> toMap() => super.toMap()..addAll({'value': value});
 }
